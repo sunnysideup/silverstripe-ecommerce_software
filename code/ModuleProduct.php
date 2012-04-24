346c05e7950e3cdfb93f54e94325c05ec38797ec
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2012-04-24 21:08:30 +0000

    BUGFIX: check if there are authors before returning first author in DDefaultMemberID
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_software/trunk@2553 950b781c-c544-efab-2f99-90083ee69683

--- code/ModuleProduct.php
+++ code/ModuleProduct.php
@@ -151,7 +151,7 @@ class ModuleProduct extends Product {
 	 */
 	public function DefaultMemberID(){
 		$memberID = 0;
-		$authors = $this->Authors()
+		$authors = $this->Authors();
 		if($authors && $authors->count()) {
 			$memberID = $authors->First()->ID;
 		}
