533f4149aa084014892d900f217baf6735542f3c
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2014-04-09 04:39:26 +0000

    UPGRADE 3.1
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_software/trunk@4923 950b781c-c544-efab-2f99-90083ee69683

--- _config.php
+++ _config.php
@@ -3,28 +3,3 @@
  * developed by www.sunnysideup.co.nz
  * Nicolaas modules [at] sunnysideup.co.nz
  **/
-
-
-//copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
-//===================---------------- START ecommerce_software MODULE ----------------===================
-//MUST SET
-//Object::add_extension('Member', 'SoftwareAuthorMemberDOD');
-//Object::add_extension("EcommerceDatabaseAdmin", "ImportModulesTask_AdminDecorator");
-//ImportModulesTask::set_data_source("/data/importme.csv");
-
-//MAY SET
-/**
- * ADD TO ECOMMERCE.YAML:
-ProductsAndGroupsModelAdmin:
-	managed_modules: [
-		...
-		ModuleProduct
-	]
-*/
-//ModuleProductGroup::remove_sort_option("default");
-//ModuleProductGroup::add_sort_option( $key = "created", $title = "Most recently added", $sql = "\"Created\" DESC");
-//ModuleProductGroup::add_sort_option( $key = "code", $title = "Code", $sql = "\"Code\" ASC");
-
-//===================---------------- END ecommerce_software MODULE ----------------===================
-
-
